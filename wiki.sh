ba5f5b79250fd4480e745267cdd64f9d73ce6648
FanFan <140507577+vindahi@users.noreply.github.com>
2024-08-06 17:46:03 +0800

    Update wiki.sh

--- wiki.sh
+++ wiki.sh
@@ -1,6 +1,5 @@
 set -e
 bits=(16 32 64 128)
-bits=(32 64)
 
 for i in ${bits[*]}; do
   CUDA_VISIBLE_DEVICES=0 python main.py --bit $i \
@@ -8,8 +7,6 @@ for i in ${bits[*]}; do
                                                      --rounds 20 \
                                                      --batch_size 64 \
                                                      --classes 10 \
-                                                     --image_dim 128 \
-                                                     --text_dim 10 \
 
 done
 
